a64f2ecc91976fafe20f043c3850419834cbd276
husseymarcos <mhussey@mail.austral.edu.ar>
2024-06-14 17:36:42 -0300

    record to class

--- src/main/java/edu/austral/ingsis/clifford/filesystem/File.java
+++ src/main/java/edu/austral/ingsis/clifford/filesystem/File.java
@@ -1,3 +1,22 @@
 package edu.austral.ingsis.clifford.filesystem;
 
-public record File(String name, Directory directory) implements FileSystem {}
+public class File implements FileSystem {
+    private final String name;
+    private final Directory directory;
+
+    public File(String name, Directory directory) {
+        this.directory = directory;
+        this.name = name;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public Directory directory() {
+        return directory;
+    }
+
+}
